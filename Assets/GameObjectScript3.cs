828751d1ad31f0bbfb2557e5c3d0ddc540f2a9b5
ksnook24 <63332685+ksnook24@users.noreply.github.com>
2021-02-25 22:32:27 -0800

    Initial commit

---
+++ Assets/GameObjectScript3.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+
+public class GameObjectScript3 : MonoBehaviour
+{
+    public string fileName = "default.txt";
+    public Text displayName;
+
+    private string[] studentNames;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadStudentNames();
+        ButtonClick();
+    }
+
+    private void LoadStudentNames()
+    {
+        this.studentNames = File.ReadAllLines(fileName);
+    }
+
+    public void ButtonClick()
+    {
+        int i = UnityEngine.Random.Range(0, this.studentNames.Length);
+        string name = this.studentNames[i];
+        displayName.text = name;
+    }
+}
